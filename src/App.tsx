d870e67aec48283e44d12db53c110f0e04dfe5e5
Diogo de Medeiros <sr.diogo@gmail.com>
2021-06-25 16:41:58 -0300

    commit 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import { createContext, useState } from 'react';
-
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/newRoom';
+import { Room } from './pages/room';
 
-export const AuthContext = createContext('');
+
+import { AuthContextProvider } from './contexts/authContext'
+import { AdminRoom } from './pages/AdminRoom';
+
+
 function App() {
-  const [user, setUser] = useState();
-
-  function signInWithGoogle() {
-    
-  }
 
   return (
     <BrowserRouter>
-    <AuthContext.Provider value={{user, setUser}}>
+    <AuthContextProvider>
+      <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/rooms/new" component={NewRoom} />
-    </AuthContext.Provider>
+    <Route path="/rooms/:id" component={Room} />
+    <Route path="/admin/rooms/:id" component={AdminRoom} />
+    </Switch>
+    </AuthContextProvider>
     </BrowserRouter>
   );
 }
